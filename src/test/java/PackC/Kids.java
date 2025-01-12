60aa43cd4cad2c2fd5e7a45369d1e7cb5b06c504
Shehla <shehlakamran16@gmail.com>
2025-01-12 16:46:10 +1100

    first commit

---
+++ src/test/java/PackC/Kids.java
@@ -0,0 +1,15 @@
+package PackC;
+
+import org.testng.annotations.Test;
+
+public class Kids {
+    @Test
+    public void eating() {
+        System.out.println("Kids want more food");
+    }
+
+    @Test
+    public void walking() {
+        System.out.println("Kids run fast");
+    }
+}
---
+++ src/test/java/packA/Sisters.java
@@ -0,0 +1,20 @@
+package packA;
+
+import org.testng.annotations.Test;
+
+public class Sisters {
+    @Test
+    public void eating() {
+        System.out.println("Girls eat less");
+    }
+
+    @Test
+    public void shopping() {
+        System.out.println("Girls love shopping");
+    }
+
+    @Test
+    public void walking() {
+        System.out.println("Girls run fast");
+    }
+}
